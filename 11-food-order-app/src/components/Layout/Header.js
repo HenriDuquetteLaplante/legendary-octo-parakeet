f3621520568ac0861177360beca2a43985c3b964
Henri Duquette-Laplante <henri.duquette-laplante@projexmedia.com>
2023-04-28 16:23:01 -0400

    138

--- 11-food-order-app/src/components/Layout/Header.js
+++ 11-food-order-app/src/components/Layout/Header.js
@@ -1,15 +1,18 @@
 import classes from "./Header.module.css";
 import React from "react";
 
+import mealsImage from '../../assets/meals.jpg';
+import HeaderCartButton from "./HeaderCartButton";
 
-const Header = () => {
+const Header = props => {
     return (
         <React.Fragment>
-            <div className={classes.header}>
+            <header className={classes.header}>
                 <h1>ReactMeals</h1>
-            </div>
+                <HeaderCartButton />
+            </header>
             <div className={classes.mainImage}>
-                <img src="/meals.jpg" alt='meal image'/>
+                <img src={mealsImage} alt='A table full of delicious food'/>
             </div>
 
 
---
+++ 11-food-order-app/src/components/Meals/Meals.js
@@ -0,0 +1,15 @@
+import React from "react";
+import AvailableMeals from "./AvailableMeals";
+import MealSummary from "./MealsSummary";
+
+const Meals = () => {
+
+    return (
+        <React.Fragment>
+            <MealSummary/>
+            <AvailableMeals />
+        </React.Fragment>
+    )
+}
+
+export default Meals;
