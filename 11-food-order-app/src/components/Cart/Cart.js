b60f66dc9d9199452886a386f67b752c2cc61ef5
Henri Duquette-Laplante <henri.duquette-laplante@projexmedia.com>
2023-05-05 15:30:24 -0400

    143.5

--- 11-food-order-app/src/components/Cart/Cart.js
+++ 11-food-order-app/src/components/Cart/Cart.js
@@ -26,7 +26,7 @@ const Cart = (props) => {
         <span>35.62</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["buttons--alt"]} onClick={props.onClose}>
+        <button className={classes['button--alt']} onClick={props.onClose}>
           Close
         </button>
         <button className={classes.button}>Order</button>
