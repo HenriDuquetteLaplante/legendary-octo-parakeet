5b50fcc19da5f7a66aa51a4a2fc9ffbeb9825adb
Henri Duquette-Laplante <henri.duquette-laplante@projexmedia.com>
2023-05-15 09:34:28 -0400

    178

---
+++ 14-sending-http-request/src/App.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+
+import MoviesList from './components/MoviesList';
+import './App.css';
+
+function App() {
+    const [movies, setMovies] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    async function fetchMoviesHandler() {
+        setIsLoading(true);
+        const response= await fetch('https://swapi.dev/api/films');
+        const jsonData = await response.json();
+
+        setMovies(jsonData.results);
+        setIsLoading(false);
+  }
+
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+          {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+          {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+          {isLoading && <p>Loading...</p>}
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
